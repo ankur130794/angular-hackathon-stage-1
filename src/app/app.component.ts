684ce9b6b0171c81ce67cd3fdb6250b0dff06e3c
Yerqueri <yayush13@gmail.com>
2017-09-19 17:26:03 +0530

    stage 2 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    timeOutFlag:boolean=false; // if true than game timeout else game running;
+    // gameStarted:boolean=false;
+    timeOutStarted:boolean=false; //if false than game timer not yet started
+    rand:number =1;
+    score:number=0;
+
+    val:number=1;
+    idbut:string='but1';
+    
+    numbers;
+    
+    constructor(){
+      this.numbers = Observable.timer(500,500); // Call after 500 second.. Please set your time
+      this.numbers.subscribe(time =>{
+        // this.GenerateRandFour();
+        // console.log(this.rand);
+        this.changeColors(this.val,this.idbut);
+        // console.log(this.val,this.idbut);
+      });
+    }
+    
+
+    StartTimeout(){
+      setTimeout(() => {
+        this.timeOutFlag=true;
+      }, 9000);
+    }
+
+    GenerateRandFour() {
+      this.rand = (Math.floor((Math.random( ) *10)+1)%4)+1;  
+    }
+
+    changeColors(quadValue:number,id:string){
+      document.getElementById(id).style.background="#000";
+      this.GenerateRandFour();
+      let elementId = this.rand.toString();
+      console.log("elementId"+elementId);
+      elementId = "but" + elementId;
+      document.getElementById(elementId).style.background="#fff";
+      this.val=this.rand;
+      this.idbut=elementId;  
+    }
+    
+
+    QuadrantClick(quadValue:number,id:string){
+      if(this.timeOutStarted===false){
+        this.StartTimeout(); //starts game timer on first function invocation;
+        this.timeOutStarted=true;
+        console.log("timeoutStarted");
+      }
+      if(this.timeOutFlag===false){
+        if(quadValue==this.rand){
+          this.score++;
+          console.log("score"+this.score);
+          this.changeColors(quadValue,id);
+        }
+        else{
+          console.log("invalid move")
+        }
+      }
+      else{
+          console.log("finalscore"+this.score);
+          console.log('your 90 secs are over');
+          this.numbers=null;
+      }
+
+    }
+
 }
